5f70f05f62e72936b2c6fc0317812c1b2289816d
Nick Golding <nick@nickgolding.design>
2025-01-22 12:05:49 +0000

    Rename POST data to limit scope to one journey

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 router.post('/choosing-actions-visualisations/simple-pick-list/answer-action-plan-external', function(request, response){
-    var actionPlanIsExternal = request.session.data["alreadyHaveActionPlan"]
+    var actionPlanIsExternal = request.session.data["choosingActions.alreadyHaveActionPlan"]
 
     if (actionPlanIsExternal == "Yes") {
         response.redirect("external-action-plan")
