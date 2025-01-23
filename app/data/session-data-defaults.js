73c7e3f6759d7223bbf7a3b7c819e05eb68b8c1c
Nick Golding <nick@nickgolding.design>
2025-01-23 17:10:29 +0000

    Set up action details in default data, and use that in the choose-actions-details page

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,28 @@
 module.exports = {
 
-  // Insert values here
+  db: {
+    actions: [
+      {
+        shortCode: 'targets',
+        title: 'Set internal targets for gender representation and equality',
+        description: 'THE DATA IS WORKING :O'
+      },
+      {
+        shortCode: 'equity-leads',
+        title: 'Appoint employment equity leads and/or task forces',
+        description: 'THE DATA IS WORKING :O'
+      },
+      {
+        shortCode: 'flexible-working',
+        title: 'Offer flexible working by default in job adverts',
+        description: 'THE DATA IS WORKING :O'
+      },
+      {
+        shortCode: 'structured-interviews',
+        title: 'Use structured interviews for recruitment and promotions',
+        description: 'THE DATA IS WORKING :O'
+      }
+    ]
+  }
 
 }
