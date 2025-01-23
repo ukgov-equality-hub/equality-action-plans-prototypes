602b344262527ce319dd62ab3a54d0ba729410e0
Nick Golding <nick@nickgolding.design>
2025-01-23 17:15:47 +0000

    Change action descriptions to be more presentable

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,22 +5,30 @@ module.exports = {
       {
         shortCode: 'targets',
         title: 'Set internal targets for gender representation and equality',
-        description: 'THE DATA IS WORKING :O'
+        description: 'Targets provide motivation and accountability, meaning that change is more likely to happen.'
+          + ' General goals such as “we will change our culture to be more inclusive” are not effective unless'
+          + ' they are accompanied by specific targets.'
       },
       {
         shortCode: 'equity-leads',
         title: 'Appoint employment equity leads and/or task forces',
-        description: 'THE DATA IS WORKING :O'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+          + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
+          + ' aliquip ex ea commodo consequat.'
       },
       {
         shortCode: 'flexible-working',
         title: 'Offer flexible working by default in job adverts',
-        description: 'THE DATA IS WORKING :O'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+          + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
+          + ' aliquip ex ea commodo consequat.'
       },
       {
         shortCode: 'structured-interviews',
         title: 'Use structured interviews for recruitment and promotions',
-        description: 'THE DATA IS WORKING :O'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+          + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
+          + ' aliquip ex ea commodo consequat.'
       }
     ]
   }
