5eb97c1e80dfb7f99791499fe70230556241dd09
Nick Golding <nick@nickgolding.design>
2025-01-28 16:05:19 +0000

    Basic structure for fixed categories

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,9 +1,37 @@
 module.exports = {
 
   db: {
+    company: {
+      name: "Dunder Mifflin Paper Company, Inc"
+    },
+
+    actionCategories: [
+      {
+        shortCode: 'leadership-and-accountability',
+        title: 'Leadership and accountability'
+      },
+      {
+        shortCode: 'recruitment',
+        title: 'Hiring and selection'
+      },
+      {
+        shortCode: 'promotion-and-retention',
+        title: 'Promotion and retention'
+      },
+      {
+        shortCode: 'flexibility',
+        title: 'Workplace flexibility'
+      },
+      {
+        shortCode: 'menopause',
+        title: 'Support during menopause'
+      }
+    ],
+    
     actions: [
       {
         shortCode: 'targets',
+        category: 'leadership-and-accountability',
         title: 'Set internal targets for gender representation and equality',
         description: 'Targets provide motivation and accountability, meaning that change is more likely to happen.'
           + ' General goals such as “we will change our culture to be more inclusive” are not effective unless'
@@ -11,6 +39,7 @@ module.exports = {
       },
       {
         shortCode: 'equity-leads',
+        category: 'leadership-and-accountability',
         title: 'Appoint employment equity leads and/or task forces',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
           + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
@@ -18,6 +47,7 @@ module.exports = {
       },
       {
         shortCode: 'flexible-working',
+        category: 'recruitment',
         title: 'Offer flexible working by default in job adverts',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
           + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
@@ -25,6 +55,7 @@ module.exports = {
       },
       {
         shortCode: 'structured-interviews',
+        category: 'recruitment',
         title: 'Use structured interviews for recruitment and promotions',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
           + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
