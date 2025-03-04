242adca1e1fbacde893b084db3645ef625a5b350
Nick Golding <nick@nickgolding.design>
2025-03-04 10:57:25 +0000

    Add data collection category and initial actions

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -61,6 +61,14 @@ module.exports = {
           + ' the organisation, which supports all other actions to have more impact.'
       },
       {
+        shortCode: 'data',
+        title: 'Data collection',
+        description: 'Collecting appropriate data and information from and about your employees'
+          + ' gives you the picture you need in order to decide which changes to make, and how.'
+          + ' The more data you have, the more nuanced your picture will be and the better your'
+          + ' changes can be targeted.'
+      },
+      {
         shortCode: 'recruitment',
         title: 'Hiring and selection'
       },
@@ -96,6 +104,36 @@ module.exports = {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
           + ' et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'
           + ' aliquip ex ea commodo consequat.'
+      },
+      {
+        shortCode: 'measure-remuneration-gap',
+        category: 'data',
+        targets: [],
+        title: 'Measure average remuneration gap women and men',
+        description: 'You already collect this information as part of your legal responsibilities.'
+          + ' This measures the difference between the average and median remuneration of women and men.'
+          + ' The gender pay gap is the difference in average earnings between women and men in the workforce.'
+          + ' The gender pay gap is not to be confused with women and men being paid the same for the same,'
+          + ' or comparable, job - this is equal pay.'
+      },
+      {
+        shortCode: 'measure-workforce-composition',
+        category: 'data',
+        targets: [],
+        title: 'Measure the gender composition of the workforce',
+        description: 'You already collect a basic breakdown of your workforce as part of your legal responsibilities.'
+          + ' However, you can enhance this information by adding rates of full-time, part-time and casual work,'
+          + ' gender split by industry and the proportion of each gender in leadership as well as the rates of resignations,'
+          + ' promotions and appointments by gender.'
+      },
+      {
+        shortCode: 'measure-flexible-working-utilisation',
+        category: 'data',
+        targets: [],
+        title: 'Measure the availability and utilisation of flexible working',
+        description: 'By understanding how many employees have access to flexible working, as well as how many of those'
+          + ' actually use it, you can understand the impact those policies are having and identify any problems that are'
+          + ' preventing people from using them.'
       },
       {
         shortCode: 'flexible-working',
