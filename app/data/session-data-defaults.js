a8e615c6c173c9bc8b47833fe8a6bfeefc38268d
James Griffiths <james@jwgsoftware.com>
2025-11-19 16:46:39 +0000

    Add new actions to prototype

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -237,39 +237,24 @@ module.exports = {
 
     actionCategories: [
       {
-        shortCode: 'leadership-and-accountability',
-        title: 'Leadership and accountability',
-        description: 'Action plans will not be effective unless there are individuals with authority'
-          + ' who are accountable for the outcome. These actions lay a foundation of accountability in'
-          + ' the organisation, which supports all other actions to have more impact.'
-      },
-      {
-        shortCode: 'data',
-        title: 'Data collection',
-        description: 'Collecting appropriate data and information from and about your employees'
-          + ' gives you the picture you need in order to decide which changes to make, and how.'
-          + ' The more data you have, the more nuanced your picture will be and the better your'
-          + ' changes can be targeted.'
-      },
-      {
-        shortCode: 'analysis',
-        title: 'Essential analysis',
-      },
-      {
         shortCode: 'recruitment',
-        title: 'Hiring and selection'
+        title: 'Recruiting staff'
       },
       {
         shortCode: 'promotion-and-retention',
-        title: 'Promotion and retention'
+        title: 'Developing and promoting staff'
       },
       {
-        shortCode: 'flexibility',
-        title: 'Workplace flexibility'
+        shortCode: 'leadership-and-accountability',
+        title: 'Building diversity into your organisation'
+      },
+      {
+        shortCode: 'data',
+        title: 'Analysing your data'
       },
       {
         shortCode: 'menopause',
-        title: 'Support during menopause'
+        title: 'Supporting your staff during menopause'
       }
     ],
 
@@ -277,135 +262,179 @@ module.exports = {
     //        for various prototype features to work correctly.
     actions: [
       {
-        shortCode: 'targets',
-        category: 'leadership-and-accountability',
-        level: 1,
-        targets: [],
-        title: 'Set internal targets for gender representation and equality'
-      },
-      {
-        shortCode: 'diversity-leads-and-task-forces',
-        category: 'leadership-and-accountability',
-        level: 1,
-        targets: [],
-        title: 'Appoint diversity leads and/or task forces'
-      },
-      {
-        shortCode: 'conduct-gender-pay-gap-analysis',
-        category: 'analysis',
-        title: 'Conduct a gender pay gap analysis'
-      },
-      {
-        shortCode: 'measure-remuneration-gap',
-        category: 'data',
-        level: 1,
-        targets: [],
-        title: 'Measure the average remuneration gap between women and men'
-      },
-      {
-        shortCode: 'measure-workforce-composition',
-        category: 'data',
-        level: 1,
-        targets: [],
-        title: 'Measure the gender composition of the workforce'
-      },
-      {
-        shortCode: 'measure-flexible-working-uptake',
-        category: 'data',
-        level: 2,
-        targets: [],
-        title: 'Measure the availability and uptake of flexible working'
-      },
-      {
-        shortCode: 'measure-promotions-by-gender',
-        category: 'data',
-        level: 2,
-        targets: [],
-        title: 'Measure the differences in promotion between women and men'
-      },
-      {
-        shortCode: 'flexible-working-in-job-adverts',
+        shortCode: 'inclusive-job-adverts',
         category: 'recruitment',
         level: 1,
-        targets: [
-          'lower-middle-quartile-percent-women',
-          'upper-middle-quartile-percent-women'
-        ],
-        title: 'Offer flexible working by default in job adverts'
+        targets: [],
+        title: 'Make job adverts inclusive',
+        summary: 'TBC'
+      },
+      {
+        shortCode: 'encourage-diverse-applications',
+        category: 'recruitment',
+        level: 1,
+        targets: [],
+        title: 'Encourage applications from diverse candidates',
+        summary: 'TBC'
+      },
+      {
+        shortCode: 'reduce-unconscious-bias-in-cv-screening',
+        category: 'recruitment',
+        level: 1,
+        targets: [],
+        title: 'Reduce unconscious bias in CV screening',
+        summary: 'TBC'
       },
       {
         shortCode: 'structured-interviews',
         category: 'recruitment',
         level: 1,
-        targets: [
-          'lower-middle-quartile-percent-women',
-          'upper-middle-quartile-percent-women'
-        ],
-        title: 'Use structured interviews for recruitment and promotions'
+        targets: [],
+        title: 'Run structured interviews',
+        summary: 'TBC'
       },
+      {
+        shortCode: 'fair-interview-outcomes',
+        category: 'recruitment',
+        level: 1,
+        targets: [],
+        title: 'Ensure interview outcomes are fair',
+        summary: 'TBC'
+      },
+      {
+        shortCode: 'flexible-working-in-job-adverts',
+        category: 'recruitment',
+        level: 1,
+        targets: [],
+        title: 'Promote flexible working arrangements in job adverts',
+        summary: 'Flexible work policies aid work-life balance, especially for those with caring roles. Advertising flexibility attracts a wider, more diverse talent pool.'
+      },
+
       {
         shortCode: 'remuneration-transparency',
         category: 'promotion-and-retention',
         level: 1,
-        targets: [
-          'median-hourly-pay-gap-percent',
-          'mean-hourly-pay-gap-percent',
-          'upper-quartile-percent-women',
-          'upper-middle-quartile-percent-women'
-        ],
-        title: 'Increase transparency to promotion, pay and reward processes'
+        targets: [],
+        title: 'Increase transparency for pay, promotion and rewards',
+        summary: 'Transparency in pay, promotion, and bonus policies ensures everyone understands how decisions are made.'
       },
       {
         shortCode: 'promotion-application-by-default',
         category: 'promotion-and-retention',
         level: 1,
-        targets: [
-          'median-hourly-pay-gap-percent',
-          'mean-hourly-pay-gap-percent',
-          'upper-quartile-percent-women',
-          'upper-middle-quartile-percent-women'
-        ],
-        title: 'Default employees into applying for promotion or other competitive opportunities'
+        targets: [],
+        title: 'Automatically put forward employees for promotion',
+        summary: 'Automatically considering all eligible employees for promotion gives them the choice to opt-out rather than opt-in.'
       },
       {
-        shortCode: 'share-local-support-for-flexible-working',
-        category: 'flexibility',
+        shortCode: 'employee-development-actionable-steps',
+        category: 'promotion-and-retention',
         level: 1,
-        targets: [
-          'median-hourly-pay-gap-percent',
-          'mean-hourly-pay-gap-percent'
-        ],
-        title: 'Share local support for parental leave and flexible working'
+        targets: [],
+        title: 'Encourage employee development through actionable steps',
+        summary: 'Asking employees for advice instead of feedback can aid skill development, and benefit organisations with low female progression and retention.'
       },
       {
-        shortCode: 'flexible-working-available-to-all',
-        category: 'flexibility',
+        shortCode: 'internships-mentoring-development-programmes',
+        category: 'promotion-and-retention',
         level: 1,
-        targets: [
-          'upper-quartile-percent-women',
-          'upper-middle-quartile-percent-women',
-          'lower-middle-quartile-percent-women'
-        ],
-        title: 'Make workplace or role adjustments available to everyone'
+        targets: [],
+        title: 'Offer internships, mentoring and other development programmes',
+        summary: 'Providing development programmes, such as internships, gives employees a formal channel for advice, support, and advocacy.'
+      },
+
+      {
+        shortCode: 'targets',
+        category: 'leadership-and-accountability',
+        level: 1,
+        targets: [],
+        title: 'Set targets to improve gender representation',
+        summary: 'Setting specific, monitorable internal targets gives your organisation clear steps to boost gender representation and equality.'
       },
       {
-        shortCode: 'rest-areas',
+        shortCode: 'diversity-leads-taskforces',
+        category: 'leadership-and-accountability',
+        level: 1,
+        targets: [],
+        title: 'Appoint diversity leads or taskforces',
+        summary: 'Appointing equality leads or committees to scrutinise hiring and progression policies ensures organisational accountability.'
+      },
+      {
+        shortCode: 'parental-leave-support',
+        category: 'leadership-and-accountability',
+        level: 1,
+        targets: [],
+        title: 'Support staff to take parental leave',
+        summary: 'Promoting clear and accessible parental leave policies boosts gender equality and helps employees balance family life with work.'
+      },
+
+      {
+        shortCode: 'data-analysis',
+        category: 'data',
+        level: 1,
+        targets: [],
+        title: 'Analyse your data to understand your pay gap and staff needs',
+        summary: 'Analysing pay data can help you understand your gender pay gap\'s scale and drivers, and choose effective actions to close the gap.'
+      },
+      {
+        shortCode: 'measure-equality-initiatives-outcomes',
+        category: 'data',
+        level: 1,
+        targets: [],
+        title: 'Measure outcomes of equality initiatives',
+        summary: 'Tracking equality initiative outcomes can measure changes and see if you\'re making progress towards your goals.'
+      },
+
+      {
+        shortCode: 'menopause-training',
         category: 'menopause',
         level: 1,
         targets: [
           'menopause'
         ],
-        title: 'Provide rest areas or quiet rooms and fans/cooling systems'
+        title: 'Train line managers to support employees experiencing menopause',
+        summary: 'Line manager training on the menopause and its effects can help organisations to better support employees experiencing the menopause.'
       },
       {
-        shortCode: 'relax-uniform-policies',
+        shortCode: 'menopause-occupational-health-advice',
         category: 'menopause',
         level: 1,
         targets: [
           'menopause'
         ],
-        title: 'Evaluate and relax uniform policies'
-      }
+        title: 'Offer occupational health advice to employees experiencing menopause',
+        summary: 'Supporting employees with specialised occupational health advice can help them manage symptoms, access support, and work more comfortably.'
+      },
+      {
+        shortCode: 'menopause-networks-support-groups',
+        category: 'menopause',
+        level: 1,
+        targets: [
+          'menopause'
+        ],
+        title: 'Set up menopause networks and support groups',
+        summary: 'Accessible menopause support groups within your organisation can help provide peer support, information, and guidance.'
+      },
+      {
+        shortCode: 'menopause-workplace-adjustments',
+        category: 'menopause',
+        level: 1,
+        targets: [
+          'menopause'
+        ],
+        title: 'Offer workplace adjustments to employees experiencing menopause',
+        summary: 'Personalised workplace adjustments help support employees experiencing menopause and wider women’s health conditions.'
+      },
+      {
+        shortCode: 'menopause-risk-assessment',
+        category: 'menopause',
+        level: 1,
+        targets: [
+          'menopause'
+        ],
+        title: 'Conduct a menopause risk assessment for your workplace',
+        summary: 'Workplace risk assessments on menopause and women\'s health can identify necessary adaptations to support employee wellbeing.'
+      },
     ],
 
     metrics: [
